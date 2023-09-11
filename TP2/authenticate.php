f0af4d129fa76426e4745196f01f3419e2a78032
Tejeddinne Khenissi <tejeddinne.khenissi@unistra.fr>
2023-09-12 01:24:52 +0200

    tp2 fini

--- TP2/authenticate.php
+++ TP2/authenticate.php
@@ -7,7 +7,7 @@ require  'bdd.php';
 $username =htmlentities($_POST['username']);
 $password = htmlentities($_POST['password']);
 
-// verify if the http request is a POST request
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
      if(array_key_exists($username,$userse) ) {
